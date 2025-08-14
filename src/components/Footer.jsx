c7a92336d26b1bb6e4bc4329352f20e1501137b5
Naufil Shaikh <141832951+naufil2002@users.noreply.github.com>
2025-08-14 17:26:23 +0530

    updatedddd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@gmail.com - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2024 - All Right Reserved.</p>
         </div>
 
     </div>
